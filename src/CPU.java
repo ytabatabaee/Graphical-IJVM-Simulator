c8e43005d82827cf9ef37241848a0512e2a155ca
m4hd1gh0 <m4hd1gh0@gmail.com>
2018-07-09 10:46:40 +0430

    sc reset connected

--- src/CPU.java
+++ src/CPU.java
@@ -16,7 +16,7 @@ public class CPU {
                     !controlUnit.write(), dataPath.getAR().getData_out(), dataPath.getDR().getData_out());
             clk++;
             boolean a = !(controlUnit.read() | controlUnit.write() | !memory.isReady());
-            controlUnit.count(reset | a, memory.isReady(), reset);
+            controlUnit.count(reset | a | controlUnit.sc_reset(memory.isReady()), memory.isReady(), reset);
 
         } while (!memory.isReady());
         return res;
@@ -31,7 +31,7 @@ public class CPU {
                 controlUnit.shift_amt(), controlUnit.AR_LD(), controlUnit.CPP_LD(), controlUnit.BSelect(),
                 controlUnit.SP_SUB4(), controlUnit.SP_ADD4(), controlUnit.SP_LD(), controlUnit.H_LD());
         clk++;
-        controlUnit.count(reset | a, memory.isReady(), reset);
+        controlUnit.count(reset | a | controlUnit.sc_reset(memory.isReady()), memory.isReady(), reset);
         controlUnit.time_signals();
     }
 
@@ -41,26 +41,10 @@ public class CPU {
         controlUnit.instruction_decoding(dataPath.IR());
         boolean a = !(controlUnit.read() | controlUnit.write() | !memory.isReady());
         clk++;
-        controlUnit.count(reset | a, memory.isReady(), reset);
+        controlUnit.count(reset | a | controlUnit.sc_reset(memory.isReady()), memory.isReady(), reset);
         controlUnit.time_signals();
         signals(reset);
     }
-//    public void signals(String data_in, boolean ready, boolean reset) {
-//        address = dataPath.memory_address();
-//        //wData = dataPath.memory_wdata();
-//        //start = dataPath.start();
-//        //rwn = dataPath.rwn();
-//        sc_value = controlUnit.sc_val();
-//        fetch();
-//        controlUnit.time_signals();
-//        controlUnit.instruction_decoding(dataPath.IR());
-//        dataPath.signals(controlUnit.AR_LD(), controlUnit.WD_LD(ready), controlUnit.DR1_LD(ready),
-//                controlUnit.DR2_LD(ready), controlUnit.PC_LD(dataPath.Z(), dataPath.N()), controlUnit.IR_LD(ready),
-//                controlUnit.SP_SUB4(), controlUnit.SP_ADD4(), reset, controlUnit.ST_val(), controlUnit.RW_val(),
-//                controlUnit.Z_en(), controlUnit.N_en(), controlUnit.PC_INC(),
-//                controlUnit.bus_sel(dataPath.Z(), dataPath.N()), controlUnit.extractor_sel(), data_in,
-//                controlUnit.ALU_D2sel(dataPath.Z(), dataPath.N()), controlUnit.ALU_control(dataPath.Z(), dataPath.N()));
-//    }
 
     public ControlUnit getControlUnit() {
         return controlUnit;
